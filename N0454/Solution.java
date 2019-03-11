f219db0169249d5ad50862d6e3f93eef4372b895
Yu Zhang <zhy@pn1806699.lanl.gov>
2019-03-10 21:55:09 -0600

    4Sum II

---
+++ N0454/Solution.java
@@ -0,0 +1,26 @@
+
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < C.length; i++) {
+           for (int j = 0; j < D.length; j++) {
+              int sum = C[i] + D[j];
+              map.put(sum, map.getOrDefault(sum, 0) + 1);
+           }
+        }
+
+        int res = 0;
+        for (int i = 0; i < A.length; i++) {
+           for (int j = 0; j < B.length; j++) {
+              int sum = - A[i] - B[j];
+              res += map.getOrDefault(sum, 0);
+           }
+        }
+        return res;
+
+    }
+}
